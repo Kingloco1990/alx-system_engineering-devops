9b82f39fa51cc2735a07663b1038807d5dada8ef
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-09-08 17:58:14 +0000

    Creates 5 zombie processes

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,12 +3,12 @@
 #include "unistd.h"
 
 /**
- * infinite_while - This function enters an infinite loop
- *                  and sleeps for 1 second in each iteration.
- *
- * Return: 0 when the loop is terminated but in practice,
- *         this function is intended to run indefinitely.
- */
+* infinite_while - This function enters an infinite loop
+*                  and sleeps for 1 second in each iteration.
+*
+* Return: 0 when the loop is terminated but in practice,
+*         this function is intended to run indefinitely.
+*/
 int infinite_while(void)
 {
 	while (1)
@@ -19,17 +19,21 @@ int infinite_while(void)
 }
 
 /**
- * main - the entry to a program that creates 5 zombie process
- *
- * Return: 0 on success
- */
+* main - The entry to a program that creates 5 zombie process.
+*
+* Return: 0.
+*/
 int main(void)
 {
 	int children = 0;
+
 	pid_t pid;
 
 	while (children < 5)
 	{
+		/* Creates a copy of the parent process and assigns it tp pid */
+		/* A copy of the parent process is a child process */
+		/* fork() returns 0 to the child process and PID to the parent process */
 		pid = fork();
 		if (pid)
 		{
@@ -41,8 +45,13 @@ int main(void)
 			exit(0);
 		}
 	}
-
+	/**
+	 * infinite_while() ensures that the parent process keeps running
+	 * indefinitely. Therefore the created child processes are not removed
+	 * from the system imediately, thus these child processes become
+	 * zombie processes.
+	 */
 	infinite_while();
 
-	return (EXIT_SUCCESS);
+	return (0);
 }
