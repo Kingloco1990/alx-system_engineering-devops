e44e148c14b1248b92f8e67563f1ce0672468a82
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-03-13 10:57:08 +0000

    Creates a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 
 file { '/tmp/school':
   ensure  => 'file',
-  path    => '/tmp/school'
+  path    => '/tmp/school',
   content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
