37ce98abbef0c2dd53deca212680d9889f2ea61c
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-02 10:10:04 +0000

    Installs and configures an Nginx web server using Puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,27 @@
+# Installs and configures an Nginx web server using Puppet.
+
+exec { 'Update package lists':
+    command => 'apt-get update',
+    path    => ['/bin', '/usr/bin', '/usr/sbin'], 
+}
+
+package { 'nginx'
+    ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+    ensure  => 'file',
+    path    => '/var/www/html/index.html',
+    content => 'Hello World!',
+    mode    => '0744',
+}
+
+exec { 'redirection configuration':
+    command => sed -i "s/location \/ {/location \/redirect_me {/; s/try_files \$uri \$uri\/ =404;/return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;/" /etc/nginx/sites-available/default,
+    path    => ['/bin', '/usr/bin', '/usr/sbin'],
+}
+
+exec { 'Restart Nginx':
+    command => 'service nginx restart',
+    path    => ['/bin', '/usr/bin', '/usr/sbin'], 
+}
