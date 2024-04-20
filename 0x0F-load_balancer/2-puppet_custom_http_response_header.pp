7cd98683be8446b8a1f96d1a56b5afdc7f9cd157
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-20 22:46:34 +0000

    Installs Nginx and configures a custom HTTP header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,17 @@
 # Installs and configures an Nginx web server using Puppet.
 
-exec {'Update':
-    command => '/usr/bin/apt-get update',
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-    ensure => installed,
+-> package { 'nginx':
+  ensure => installed,
 }
-
-file_line { 'header_served_by':
-    path  => '/etc/nginx/sites-available/default',
-    match => '^server {',
-    line  => "server {\n\tadd_header X-Served-By \"${hostname}\";",
-    multiple => false,
+-> file_line { 'header_served_by':
+  path  => '/etc/nginx/sites-available/default',
+  match => '^server {',
+  line  => "server {\n\tadd_header X-Served-By \"${hostname}\";",
+  multiple => false,
 }
-
-exec { 'Restart Nginx':
-    command => 'service nginx restart',
-    path    => ['/bin', '/usr/bin', '/usr/sbin'],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
