951041187f759917662f4ee3294b7c98a75e7cbe
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-20 21:56:27 +0000

    Installs Nginx and configures a custom HTTP header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,18 @@
 # Installs and configures an Nginx web server using Puppet.
 
-# Install the Nginx package
 package { 'nginx':
-    ensure => installed, # Ensure that the package is installed
+    ensure => installed,
 }
 
-# Use exec resource to substitute text in the NGINX configuration file
-exec { 'custom HTTP header':
-    environment => ["HOST=${hostname}"], # Set environment variable for the hostname
-    command => 'sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n\n\tadd_header X-Served-By $HOST;/" /etc/nginx/sites-available/default', # Command to substitute text in NGINX config file
-    path    => ['/bin', '/usr/bin', '/usr/sbin'], # Set the path for command execution
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
 # Define an Exec resource to restart Nginx service
 exec { 'Restart Nginx':
-    command => 'service nginx restart', # Command to restart Nginx service
-    path    => ['/bin', '/usr/bin', '/usr/sbin'], # Set the path for command execution
+    command => 'service nginx restart',
+    path    => ['/bin', '/usr/bin', '/usr/sbin'],
 }
