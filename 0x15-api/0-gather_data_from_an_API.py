e908b297819aee8526dea32b19b41a554f0d3b97
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-07-03 18:15:05 +0000

    Returns to-do list info for a given employee ID

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,37 @@
-#!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Fetches and displays completed to-do list information for a specified user ID.
+
+This script retrieves data from the JSONPlaceholder API
+(https://jsonplaceholder.typicode.com/) to fetch the user details and their
+corresponding to-do list based on the provided user ID.
+
+Usage:
+    $ python script_name.py <user_id>
+"""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    # API base URL
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    # User ID from command-line argument
+    user_id = sys.argv[1]
+
+    # Fetch user details
+    user = requests.get(url + 'users/{}'.format(user_id)).json()
+
+    # Fetch to-do list for the user
+    params = {'userId': user_id}
+    todo_list = requests.get(url + 'todos', params=params).json()
+
+    # Filter completed tasks
+    comp_tasks = [todo.get(
+        'title') for todo in todo_list if todo.get('completed') is True]
+
+    # Print summary of completed tasks
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user.get('name'), len(comp_tasks), len(todo_list)))
+
+    # Print each completed task title
+    [print('\t {}'.format(comp)) for comp in comp_tasks]
