4f9d60c7d98958d5f860c5f3ab7f1e0f1c0d0dde
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-07-03 19:18:49 +0000

    Exports data of all employees into a JSON format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+"""
+Script to fetch and export to-do list information for all users to a JSON file.
+
+This script retrieves data from the JSONPlaceholder API
+(https://jsonplaceholder.typicode.com/) to fetch details of all users
+and their corresponding to-do lists.
+
+The retrieved to-do lists are then exported to a JSON file named
+`todo_all_employees.json`.
+
+Usage:
+    $ python3 script_name.py
+"""
 import json
 import requests
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
+    # Base URL for the API
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    with open("todo_all_employees.json", "w") as jsonfile:
+    # Fetch details of all users
+    users = requests.get(url + 'users').json()
+
+    # Write to-do list of all users to a
+    # JSON file named `todo_all_employees.json`
+    with open("todo_all_employees.json", "w") as file:
         json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+            user.get('id'): [{
+                'username': user.get('username'),
+                'task': todo.get('title'),
+                'completed': todo.get('completed')
+            } for todo in requests.get(
+                url + 'todos', params={'userId': user.get('id')}).json()]
+            for user in users
+        }, file)
